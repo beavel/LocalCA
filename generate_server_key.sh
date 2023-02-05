c29b8ad998f5a2bd93e810845721c7c29f6bc5c1
beavel <beavel@outlook.com>
2023-02-05 13:39:24 -0500

    Add support for generatin RSA server keys

--- generate_server_key.sh
+++ generate_server_key.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-while getopts ":c:o:" opt; do
+while getopts ":a:c:o:" opt; do
   case ${opt} in
+    a) ALGORITHM=$OPTARG
+    ;;
     c) OPENSSL_CONFIG=$(readlink -f "$OPTARG")
     ;;
     o) SERVER_REQ=$OPTARG
@@ -13,19 +15,37 @@ while getopts ":c:o:" opt; do
 done
 
 # Configure defaults for inputs
+if [[ -z $ALGORITHM ]]; then
+    ALGORITHM=ECC
+fi
+
+if [ "$ALGORITHM" = "ECC" ]; then
+  KEY_NAME=ecc_key
+fi
+
+if [ "$ALGORITHM" = "RSA" ]; then
+  KEY_NAME=rsa_key
+fi
+
 if [[ -z $OPENSSL_CONFIG ]]; then
     OPENSSL_CONFIG=configs/openssl_req.cnf
 fi
 
 if [[ -z $SERVER_REQ ]]; then
-    SERVER_REQ=tls/ecc_key.csr
+    SERVER_REQ=tls/${KEY_NAME}.csr
 fi
 
 # Files
-SERVER_KEY=tls/private/ecc_key.pem
+SERVER_KEY=tls/private/${KEY_NAME}.pem
 
 mkdir -p tls/certs tls/private
 
-openssl ecparam -genkey -name prime256v1 -out ${SERVER_KEY}
+if [ "$ALGORITHM" = "ECC" ]; then
+  openssl ecparam -genkey -name prime256v1 -out ${SERVER_KEY}
+fi
+
+if [ "$ALGORITHM" = "RSA" ]; then
+  openssl genrsa -out ${SERVER_KEY} 4096
+fi
 
 openssl req -new -sha256 -key ${SERVER_KEY} -out ${SERVER_REQ} -config ${OPENSSL_CONFIG}
